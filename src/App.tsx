e7482242e47d615380f3847c0d4421856f508679
Rancis <thangld@baobinh.net>
2021-04-14 10:54:46 +0700

    get token success

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,21 @@ import {LoginButton, AccessToken} from 'react-native-fbsdk';
 
 const App: React.FC = (): React.ReactElement => {
   return (
-    <SafeAreaView>
-      <Text>Demoss</Text>
-      <LoginButton />
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('-> error: ', error);
+          } else if (result.isCancelled) {
+            console.log('-> result isCancelled ');
+          } else {
+            AccessToken.getCurrentAccessToken().then(data => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+      />
     </SafeAreaView>
   );
 };
