28da2f0e2be355a03db27b1987c2ecedca50d18a
Rancis <thangld@baobinh.net>
2021-04-14 09:27:32 +0700

    change to tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App: React.FC = (): React.ReactElement => {
+  return (
+    <SafeAreaView>
+      <Text>Demo</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
